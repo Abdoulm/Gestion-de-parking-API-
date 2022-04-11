4da4f042822eb5ccb70cd0f5c828814ab71ad2b4
Abdoulm <abdelshmurda@gmail.com>
2022-04-11 05:47:11 +0200

    api commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express")
+const app = express()
+const parkings = require('./parkings.json')
+app.listen(8080, () => {
+    console.log("Serveur à l'écoute")
+});
+
+app.get("/parkings", (req, res)=>{
+    res.status(200).json(parkings)
+});
+
+app.get("/parkings/:id", (req,res)=>{
+    const id = parseInt(req.params.id);
+    const parking = parkings.find(parking => parking.id === id);
+    res.status(200).json(parking)
+})
+
